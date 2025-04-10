76d296d518dfffa32bedd6dd4079e8c4c6e75b07
rajan <73783957+RK-41@users.noreply.github.com>
2025-04-10 12:27:47 +0530

    Bugfix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	resolve: {
